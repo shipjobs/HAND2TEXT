3be164d32070107fea7d39d7d592e385c862b37d
In Sung <50741766+shipjobs@users.noreply.github.com>
2021-06-16 18:09:20 +0900

    Update json_Ctrl.py

--- myClass/json_Ctrl.py
+++ myClass/json_Ctrl.py
@@ -1,72 +1,33 @@
 import json
 
-file_path = "./sample.json"
 
-###########################################################################
-#작성자 : insung-lee
-#작성일 : 2021-06-10
-#목적 : 주어진 손글씨 Json 파일 관리
-#내  용 : 일기, 저장, 수정 
-#  - 손글씨 이미지 관리 Json 파일 디자인
-###########################################################################
-
-#진행중.............전체 글씨 분석후 카테고리 정리되면 추후정리
-data = {}
-data['WORD'] = []  # 하위 1
-data['WORD'].append({   #하위 2-1
-    "SVG URL": "XXXXXXXXXXXXXXXXX",
-    "SVG FilePath": "XXXXXXXXXXXXXXXXX",
-    "PNG URL": "XXXXXX"
-})
-
-data['WORD'].append({   #하위 2-1
-    "TEXT URL": "XXXXXXXXXXXXXXXXX",
-    "TEXT FilePath": "XXXXXXXXXXXXXXXXX"
-})
-#print(data)
-
-########################################################################    
-#Json 쓰기
-def json_write(file_path):
-    #Json 쓰기모드로 열기 - 쓰기
-    with open(file_path, 'w') as outfile:
-        #json.dump(data, outfile)
-        json.dump(data, outfile, indent=4)  #indent 옵션
+#손글씨 이미지의 Json Class 
 
 
-
-########################################################################    
-#Json 읽기
-def json_read(file_path):
-    #file_path = "./sample.json"
-    with open(file_path, "r") as json_file:
-        json_data = json.load(json_file)  #읽기
-        
-        """   
-        print(json_data)
-        print("")
-        print(json_data['WORD'])
-        print("")
-        #print(json_data['WORD'][0]['XXXX'])
-        """
-"""
-json_data['posts'].append({
-    "title": "How to parse JSON in android",
-    "url": "https://codechacha.com/ko/how-to-parse-json-in-android/",
-    "draft": "true"
-})
-"""
-def json_add(file_path , json_data):
-########################################################################
-#기존 JSON 파일에 내용 추가해서 다시 저장
-    #file_path = "./sample.json"
-    json_data = {}
+# - 손글씨 관련 Josn 파일을 넣어 주면.. 해당 페이지내 글씨의 영역 좌표  (좌상단, 우하단) 를 반환
+# - 손글씨 Json 에 대한  약속된 필드 정보를 가지고 있어 야함  (정책 수립 필요)   
+def get_vertices_list_from_json( URL_json ):
     
-    #값을 불러 오기
-    with open(file_path, "r") as json_file:
+    json_data = {}  #담을 공간
+    dic_bounding_lt_rb = []
+
+    #json_file (TEST IO Wraper) 으로 지정된 경로의 파일 읽어 오기
+    with open(URL_json, "r") as json_file:  
         json_data = json.load(json_file)
 
-    #덥어 쓰기
-    with open(file_path, 'w') as outfile:
-        json.dump(json_data, outfile, indent=4)
+    #좌표 정보 진입 경로
+    #print(json_data['images'][0]['fields'][0]['boundingPoly']['vertices'][0])
+    #print(json_data['images'][0]['fields'][1]['boundingPoly']['vertices'][2])
+    
+    cnt =  len( json_data['images'][0]['fields'][0] )  #개수 가져오기
+    
+    for index_cnt , vertices in enumerate( json_data['images'][0]['fields']  ) :
+        
+        #bounding box 별도로 담아 두기
+        dic_bounding_lt_rb.insert(index_cnt , [vertices['boundingPoly']['vertices'][0]  ,    vertices['boundingPoly']['vertices'][2]])
+        print( vertices['inferText'] )
+    
+    return dic_bounding_lt_rb
 
+#get_vertices_list_from_json( "./sample_data/sample.json" )
+print( get_vertices_list_from_json( "./sample_data/sample.json" ))
