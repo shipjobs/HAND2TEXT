89bff06dec8fec9978914c416f6f2d1350361ec0
In Sung <50741766+shipjobs@users.noreply.github.com>
2021-06-23 16:43:23 +0900

    Update Image_Ctrl.py

--- myClass/Image_Ctrl.py
+++ myClass/Image_Ctrl.py
@@ -1,42 +1,55 @@
+###########################################################################
+# 작성자 : insung-lee
+# 작성일 : 2021-06-10
+# 목  적 : 이미지 처리 모듈
+# 활  용 lib : 
+#  - Pillow : https://pillow.readthedocs.io/en/stable/
+#  - OpenCV : https://docs.opencv.org/master/ 
+#  - PIL : https://pillow.readthedocs.io/en/stable/handbook/tutorial.html
+#  - Scikit-image : https://scikit-image.org/docs/stable/index.html 
+###########################################################################
+
 from PIL import Image
 
-# file_output_path = 'C:\\Mydata\\svg2png\\'  #$png 변환
-# img = Image.open( file_output_path + "0.png")
 
-# # area = (x1, y1, x2, y2)
-# cropping_area = (441,374,518,411)   #좌표 지정
-# cropped_img = img.crop(cropping_area)
-# cropped_img.save('C:\\Mydata\\word_root\\0png_1.png')
-#참조 :  https://blog.naver.com/khw11044/222003896258
-
-
-# dic_bounding_lt_rb = 좌상, 우하 [{'x': 556.0, 'y': 343.0}, {'x': 556.0, 'y': 343.0}]
-def save_text_boundingbox_from_png( source_image_url ,dic_bounding_lt_rb :dict , file_output_folder_path , file_name ):
+#PNG 파일에서 지정된 영역 잘라내서 저장 하기
+# - source_image_url : 이미지 소스 URL
+# - dic_bounding_lt_rb : 딕션어리 (텍스트 영역 좌표 모음)
+# - file_output_folder_path : 저장하고자 하는 폴더 경로 URL 
+# - parent_file_name : 잘나낸 파일의 부모 페이지 파일명
+def save_text_boundingbox_from_png( source_image_url ,dic_bounding_lt_rb :dict , file_output_folder_path , parent_file_name ):
 
     img = Image.open( source_image_url )  #text 값을 파일명으로
 
-    # area = (x1, y1, x2, y2)
-    #cropping_area = (441,374,518,411)   #좌표 지정
-    #cropping_area = ( dic_bounding_lt_rb[0] ,374,518,411)   #좌표 지정
-    cropping_area = ( dic_bounding_lt_rb[0]['x'],
-                    dic_bounding_lt_rb[0]['y'],
-                    dic_bounding_lt_rb[1]['x'],
-                    dic_bounding_lt_rb[1]['y'])                    
+    cropping_area = ( dic_bounding_lt_rb[0]['x'] - 15 ,
+                    dic_bounding_lt_rb[0]['y'] - 15 ,
+                    dic_bounding_lt_rb[1]['x']+ 15 ,
+                    dic_bounding_lt_rb[1]['y']+ 15 )                    
     
     cropped_img = img.crop(cropping_area)
-    cropped_img.save("C:\\Mydata\\word_root\\" + file_name + ".png")
+    
+    file_name = dic_bounding_lt_rb[2] + "@" + parent_file_name  ##파일명 : 텍스트@부모파일명
 
-from json_Ctrl import *
-
-dic_bounding_lt_rb = []
-dic_bounding_lt_rb = get_vertices_list_from_json("./sample_data/sample.json" )
-
-source_image_url        = "C:\\Mydata\\svg2png\\0.png"
-file_output_folder_path = "C:\\Mydata\\word_root\\"   
-
-# png 이미지 경로 , 텍스트 영역 좌표 ,  저장 경로, 저장_파일명 (텍스트 이름으로 저장) 
+    return cropped_img  , file_name   #잘라낸 이미지  , 파일명
 
 
-for index_cnt , vertices in enumerate( dic_bounding_lt_rb ) :
-    #save_text_boundingbox_from_png( source_image_url , dic_bounding_lt_rb[0]  , file_output_folder_path , "HELLO_WORLD0")
-    save_text_boundingbox_from_png( source_image_url , vertices   , file_output_folder_path , str(index_cnt)) 
+####################################################
+# 모듈 테스트 , 작업 영역
+####################################################
+# from json_Ctrl import *
+# # 임시
+# dic_bounding_lt_rb = [] 
+# #dic_bounding_lt_rb (No, [좌상 좌표 , 우하 좌표, 텍스트])
+# dic_bounding_lt_rb = get_vertices_list_from_json("./sample_data/sample.json" )
+
+# image_name = "7.png"
+# source_image_url        = "C:\\Mydata\\svg2png\\"   + image_name
+# file_output_folder_path = "C:\\Mydata\\word_root\\"   
+
+
+# # png 이미지 경로 , 텍스트 영역 좌표 ,  저장 경로, 저장_파일명 (텍스트 이름으로 저장) 
+# for index_cnt , vertices in enumerate( dic_bounding_lt_rb ) :
+#     cropped_img , file_name = save_text_boundingbox_from_png( source_image_url , vertices   , file_output_folder_path , image_name )   
+#     cropped_img.save("C:\\Mydata\\word_root\\" + file_name + ".png")  #파일명 : 텍스트_부모파일명 * 부모파일은 참조용
+
+
