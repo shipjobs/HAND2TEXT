1e610d2cc4f47abe1ac8fcc5314d16397456d437
In Sung <50741766+shipjobs@users.noreply.github.com>
2021-06-23 16:43:41 +0900

    Update json_Ctrl.py

--- myClass/json_Ctrl.py
+++ myClass/json_Ctrl.py
@@ -1,8 +1,6 @@
 import json
 
-
 #손글씨 이미지의 Json Class 
-
 
 # - 손글씨 관련 Josn 파일을 넣어 주면.. 해당 페이지내 글씨의 영역 좌표  (좌상단, 우하단) 를 반환
 # - 손글씨 Json 에 대한  약속된 필드 정보를 가지고 있어 야함  (정책 수립 필요)   
@@ -15,19 +13,33 @@ def get_vertices_list_from_json( URL_json ):
     with open(URL_json, "r") as json_file:  
         json_data = json.load(json_file)
 
-    #좌표 정보 진입 경로
+    #참고 : 좌표 정보 진입 경로
     #print(json_data['images'][0]['fields'][0]['boundingPoly']['vertices'][0])
     #print(json_data['images'][0]['fields'][1]['boundingPoly']['vertices'][2])
     
-    cnt =  len( json_data['images'][0]['fields'][0] )  #개수 가져오기
-    
-    for index_cnt , vertices in enumerate( json_data['images'][0]['fields']  ) :
+    try:
+        cnt =  len( json_data['images'][0]['fields'][0] )  #개수 가져오기
         
-        #bounding box 별도로 담아 두기
-        dic_bounding_lt_rb.insert(index_cnt , [vertices['boundingPoly']['vertices'][0]  ,    vertices['boundingPoly']['vertices'][2]])
-        print( vertices['inferText'] )
+        for index_cnt , vertices in enumerate( json_data['images'][0]['fields']  ) :
+            
+            #bounding box (좌상, 우하) , 텍스트 값 담아 두기
+            dic_bounding_lt_rb.insert(index_cnt , [vertices['boundingPoly']['vertices'][0]  ,    vertices['boundingPoly']['vertices'][2] , vertices['inferText']]  )
+            #print( vertices['inferText'] )
+
+    except TypeError as e:
+        print("for json_path in read_jsons :  TypeError [ " , e , " ] \n")
     
+    except IndexError as e:
+        print("for json_path in read_jsons :  IndexError [ " , e , " ] \n")
+                
     return dic_bounding_lt_rb
 
 #get_vertices_list_from_json( "./sample_data/sample.json" )
 print( get_vertices_list_from_json( "./sample_data/sample.json" ))
+
+
+
+
+
+
+ 
